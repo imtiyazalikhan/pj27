7ec98aaa4e6b6e29a0c2ec8a1f257545164b52c5
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2020-06-05 07:06:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var boxIMG, boxSprite
-var ground
+var ground,rope
 
 function preload()
 {
@@ -19,7 +19,7 @@ function setup() {
 
 	ball = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
 	//World.add(world, packageBody);
-	log=new log(130,150,30,10);
+	log=new Log(130,150,30,10);
 	
 	ball=new ball(150,350,20,20);
 	rope= new rope(ball.body,log.body);
