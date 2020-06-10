75194889d06dba2e4bf7663c52cb1546e13cc958
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2020-06-10 10:56:13 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -1,19 +1,32 @@
-class Rope{
-    constructor (bodyA,bodyB){
-        var option={
-            bodyA:bodyA,
-            bodyB:bodyB,
-            stiffness:0.04,
-            length:10,
+class rope{
+	constructor(body1,body2, offsetX, offsetY)
+	{
+		this.offsetX=offsetX
+		this.offsetY=offsetY
+		var options={
+			bodyA:body1,
+			bodyB:body2,
+			pointB:{x:this.offsetX, y:this.offsetY}
+		}
+		//console.log(options);
+		this.rope=Constraint.create(options)
+		World.add(world,this.rope)
+	}
 
-        }
-        this.rope=Constraint.create(option);
-        World.add(world,this.rope);
-    }
-    display(){
-        var pointA=this.rope.bodyA.position;
-        var pointB=this.rope.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x,pointA.y,pointB.x,pointB.y);
-    }
+	display()
+	{
+		var pointA=this.rope.bodyA.position;
+		var pointB=this.rope.bodyB.position;
+
+		strokeWeight(2);
+
+		var Anchor1X=pointA.x
+		var Anchor1Y=pointA.y
+
+		var Anchor2X=pointB.x+this.offsetX
+		var Anchor2Y=pointB.y+this.offsetY
+
+		line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
+	}
+
 }
